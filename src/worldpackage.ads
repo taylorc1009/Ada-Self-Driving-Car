6cca3f99a39887bfb8d70347a4cd3f4aff433998
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-04-22 10:48:53 +0100

    Fixed changeGear precondition

--- src/worldpackage.ads
+++ src/worldpackage.ads
@@ -54,16 +54,16 @@ package WorldPackage with SPARK_Mode is
      Depends => (car => (car, gear, world)),
      Pre => car.engineOn
      and gear /= car.gear
+     and (car.speed = 0
+          or (car.speed > 0 and gear = PARKED))
      and (car.parkRequested
           xor world.obstructionPresent
           xor car.forceNeedsCharged)
-     and not (car.speed < 0
-              or car.diagnosticsOn
+     and not (car.diagnosticsOn
               or (world.obstructionPresent and (car.speed /= 0 or gear /= REVERSING))
               or (car.forceNeedsCharged and (car.speed /= 0 or gear /= PARKED))
               or (gear /= PARKED and (car.battery <= MINIMUM_BATTERY
-                                      or (car.parkRequested and car.speed = 0)
-                                      or car.speed > 0))),
+                                      or (car.parkRequested and car.speed = 0)))),
      Contract_Cases => (car.speed > 0 and gear = PARKED => car.parkRequested and car.gear = car.gear'Old,
                         world.obstructionPresent => car.gear = REVERSING and car.speed = 0,
                         (car.forceNeedsCharged or car.parkRequested) and car.speed = 0 => car.gear = PARKED,
